16e0b3b44b1d5146fe3f253106eca8548b63a67a
Dhiren Jaypal <dhirennjaypal@gmail.com>
2020-08-30 21:45:46 +0530

    List of members table added

--- Database.php
+++ Database.php
@@ -9,6 +9,9 @@ class Database{
     public static function query($query){
         return mysqli_query(Database::getConnection(),$query);
     }
+	public static function selectall($table){
+        return mysqli_query(Database::getConnection(),"select * from $table;");
+    }
 }
 
 class Plans{
@@ -20,6 +23,18 @@ class Plans{
     }
 	public static function update($id,$name, $daysCount, $maxBooks, $amount, $maxIssueDuration, $penaltyAmount){
         return mysqli_query(Database::getConnection(),"update plans set name='$name',daysCount=$daysCount,maxBooks=$maxBooks,amount=$amount,maxIssueDuration=$maxIssueDuration,penalty=$penaltyAmount where id=$id");
+    }
+}
+
+class Members{
+	public static function insert($name, $address, $phone, $emailAddress, $isActive){
+        return mysqli_query(Database::getConnection(),"insert into members (name, address, phone, emailAddress, isActive) values('$name','$address','$phone','$emailAddress','$isActive')");
+    }
+	public static function delete($id){
+        return mysqli_query(Database::getConnection(),"delete from members where id=$id");
+    }
+	public static function update($id, $name, $address, $phone, $emailAddress, $isActive){
+        return mysqli_query(Database::getConnection(),"update members set name='$name', address=$address, phone=$phone, emailAddress=$emailAddress, isActive=$isActive where id=$id");
     }
 }
 
--- viewData.php
+++ viewData.php
@@ -17,17 +17,18 @@
 <?php
 if(!class_exists('Database') and !class_exists('Plans'))
 	require("./Database.php");
-use dboperations\helper\Plans as plans;
 use dboperations\helper\Database as db;
+use dboperations\helper\Plans as plans;
+use dboperations\helper\Members as users;
 
-if(isset($_GET["op"])){
-	if($_GET["op"]=="remove");
+if(isset($_GET["type"])){
+	if($_GET["type"]=="plan"){
 	$result=plans::delete($_GET["id"]);
-	if(!$result){
+	if(!$result)
 		echo "Something went wrong";
 	}
 }
-    $result=db::query("select * from plans");
+    $result=db::selectall("plans");
     echo "<table class='table table-bordered table-primary table-responsive p-4'>";
     echo //"<caption class='text-white'>List of plans</caption>
 			"<tr class='bg-primary text-white'>
@@ -52,11 +53,63 @@ if(isset($_GET["op"])){
     			<td>".$row['amount']."</td>
     			<td>".$row['maxIssueDuration']."</td>
     			<td>".$row['penalty']."</td>
-    			<td><a class='btn btn-danger' href='./viewData.php?op=remove&id=".$row['id']."'>Remove</a></td>
+    			<td><a class='btn btn-danger' href='./viewData.php?type=plan&id=".$row['id']."'>Remove</a></td>
     			<td><a class='btn btn-info' href='./updatePlans.php?id=".$row['id']."'>Update</a></td>
     		   </tr>";
     }
     echo "</table>";
+?>
+</div>
+
+
+<!-- Table for members  -->
+<div class="card bg-info rounded-lg m-5">
+<div class="card-header bg-primary text-white text-center">
+<h2>List of Members</h>
+<a class="btn btn-info" href='./createPlans.php' >Insert New Plan</a>
+</div>
+<?php
+
+if(isset($_GET["type"])){
+	if($_GET["type"]=="user");{
+		$result=users::delete($_GET["id"]);
+	if(!$result)
+		echo "Something went wrong";
+	}
+}
+$result=db::selectall("members");
+echo "<table class='table table-bordered table-primary table-responsive p-4'>";
+echo //"<caption class='text-white'>List of plans</caption>
+"<tr class='bg-primary text-white'>
+<th>#</th>
+<th>Name</th>
+<th>Address</th>
+<th>Phone</th>
+<th>Email Address</th>
+<th>Is active</th>
+<th>Delete</th>
+<th>Update</th>
+</tr>";
+$i=0;
+while($row=mysqli_fetch_assoc($result)){
+	$i=$i+1;
+	echo "<tr>
+	<td>".$i."</td>
+	<td>".$row['name']."</td>
+	<td>".$row['address']."</td>
+	<td>".$row['phone']."</td>
+	<td>".$row['emailAddress']."</td>
+	<td>";
+	if($row['isActive']=='YES')
+		echo "<input type='checkbox' checked disabled>";
+	else
+		echo "<input type='checkbox' disabled>";
+	echo "</td>
+	<td><a class='btn btn-danger' href='./viewData.php?type=user&id=".$row['id']."'>Remove</a></td>
+	<td><a class='btn btn-info' href='./updatePlans.php?id=".$row['id']."'>Update</a></td>
+	</tr>";
+}
+echo "</table>";
 ?>
 </div>
 </body>
